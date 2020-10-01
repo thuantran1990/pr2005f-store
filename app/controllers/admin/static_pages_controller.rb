055d99fbd5030de5da6c8e224b6d7b967f80359f
YOUR NAME <YOUR@EMAIL.com>
2020-10-01 13:41:41 +0700

    admin_template

---
+++ app/controllers/admin/static_pages_controller.rb
@@ -0,0 +1,5 @@
+class Admin::StaticPagesController < Admin::ApplicationController 
+  def home
+  	
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
  	root 'pages#index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+ 	namespace :admin do
+        get 'static_pages/home'
+        
+    end
 end
